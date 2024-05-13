ff2cd4209e86ce657458979aae012f02d5108e28
vuyiswa <vpalesa23@gmail.com>
2024-05-13 04:49:04 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#using puppet to make changes to config a file
+
+first_line { 'Turn off passwd auth':
+ ensure => 'present',
+ path => '/etc/ssh/ssh_config',
+ line => '    PasswordAuthentication no'.
+}
+
+file_line { 'Declare identity file':
+ ensure => 'present'
+ path => '/etc/ssh/ssh_config',
+ line => '   IdentiyFile ~/.ssh/school',
+}
